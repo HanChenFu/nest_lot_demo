7d7201b84bf6dd17be572be067458a835ec8ebab
Administrator <Administrator@PC-20191122SYUK>
2019-12-11 17:09:04 +0800

    zlh NewsController参数接收要用对象接收,为了postMan统一去调用,传参过来都是用json格式的数据

--- src/main/java/com/hc/controller/index/NewsController.java
+++ src/main/java/com/hc/controller/index/NewsController.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hc.para.page_base.BasePara;
 import com.hc.service.TbAreaDynamicsService;
 import com.hc.service.TbEmergencyNewsService;
 import com.hc.service.TbNoticeService;
@@ -29,9 +30,10 @@ public class NewsController {
 	private TbEmergencyNewsService 	tbEmergencyNewsService; 
 	
 	@RequestMapping("/everyAreaDynamics")
-	public List<T> queryEveryAreaDynamics(@RequestParam("type") String type) throws Exception {
-		System.out.println("-*********************************-" + type);
-		if(type==null || type=="") {
+	public List<T> queryEveryAreaDynamics(@RequestBody BasePara para) throws Exception {
+		int type = para.getType();
+		System.out.println("-*********************************-" + para.getType());
+		if(type==0) {
 			return tbWorkDynamicsService.queryWorkDynamics();
 		}else {
 			if(Integer.valueOf(type)==1) {
