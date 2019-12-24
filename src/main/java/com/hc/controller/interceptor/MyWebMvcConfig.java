95b414afc250eb6b6d8a71a093ea6d9a160eec41
Administrator <Administrator@PC-20191122SYUK>
2019-12-24 09:39:37 +0800

    zlh 静态资源配置

---
+++ src/main/java/com/hc/controller/interceptor/MyWebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.hc.controller.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+ 
+/**
+ * 静态资源映射
+ */
+@Configuration
+public class MyWebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/t/**")
+                .addResourceLocations("file:D:/t/");
+    }
+}
+
